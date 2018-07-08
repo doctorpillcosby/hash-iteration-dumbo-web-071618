e2ae249d8a18c282c62dc2ba0d279abf185995bf
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-08 04:58:06 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,15 +4,6 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
- inner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
- 
-winner  
-end
+
 
 
